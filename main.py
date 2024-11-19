493e1f94d0b22c3bf2841f6618419fb878facaa9
Josia Silalahi <130029208+silalahi20@users.noreply.github.com>
2024-11-19 15:06:43 +0700

    push

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello from FastAPI!. This is Simple API Deploymeent by Josia 18222075"}
+    return {"message": "Hello from FastAPI!. This is Simple API Deployment by Josia 18222075"}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
